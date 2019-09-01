008ff5aad60512873c698136d04c8f493e3d2e7a
arvindkore <arvind_kore@live.in>
2019-09-01 04:06:09 -0700

    Button Added

--- app/src/main/java/com/arv/demoandroidproject/MainActivity.kt
+++ app/src/main/java/com/arv/demoandroidproject/MainActivity.kt
@@ -2,11 +2,14 @@ package com.arv.demoandroidproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button.setOnClickListener { Toast.makeText(this@MainActivity,"Hello Friend",Toast.LENGTH_LONG).show() }
     }
 }
